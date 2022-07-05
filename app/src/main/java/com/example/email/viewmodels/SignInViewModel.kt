aa9031a6818202f834fe5044ca2897ab17a90411
Skipancho <ctg1770a@gmail.com>
2022-07-05 22:21:30 +0900

    SignInViewModel 생성

---
+++ app/src/main/java/com/example/email/viewmodels/SignInViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.email.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+class SignInViewModel(app : Application) : AndroidViewModel(app) {
+    val nickname = MutableLiveData("")
+}
