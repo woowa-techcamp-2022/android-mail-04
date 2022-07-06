a82e6c680e611728b279fbb3ffa5d4c6d6c27cc0
Skipancho <ctg1770a@gmail.com>
2022-07-06 10:59:10 +0900

    MailAdapter(recyclerView Adapter) 작성

---
+++ app/src/main/java/com/example/email/adapters/MailAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.email.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.email.R
+import com.example.email.data.Mail
+import com.example.email.databinding.MailItemBinding
+
+class MailAdapter : RecyclerView.Adapter<MailAdapter.MailItemViewHolder>() {
+
+    private val mailList = mutableListOf<Mail>()
+
+    class MailItemViewHolder(
+        private val binding : MailItemBinding
+    ) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item : Mail){
+            binding.mail = item
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MailItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.mail_item,parent,false)
+        val binding = MailItemBinding.bind(view)
+        return MailItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MailItemViewHolder, position: Int) {
+        holder.bind(mailList[position])
+    }
+
+    override fun getItemCount(): Int  = mailList.size
+}
---
+++ app/src/main/java/com/example/email/data/Mail.kt
@@ -0,0 +1,8 @@
+package com.example.email.data
+
+data class Mail(
+    val profileImgPath : String,
+    val sender : String,
+    val title : String,
+    val content : String
+)
