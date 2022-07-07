96ce5630431b113a2831e505727221337c4a2d67
Skipancho <ctg1770a@gmail.com>
2022-07-07 15:54:08 +0900

    MailAdapter.kt 주석 작성

--- app/src/main/java/com/example/email/adapters/MailAdapter.kt
+++ app/src/main/java/com/example/email/adapters/MailAdapter.kt
@@ -9,18 +9,34 @@ import com.example.email.R
 import com.example.email.data.Mail
 import com.example.email.databinding.MailItemBinding
 
+/**
+ * recyclerView 와 Mail 목록을 연결해줄 Adapter
+ */
 class MailAdapter : RecyclerView.Adapter<MailAdapter.MailItemViewHolder>() {
 
+    /**
+     * ViewHolder, View 에 데이터를 입력?한다.
+     */
     class MailItemViewHolder(
         private val binding : MailItemBinding
     ) : RecyclerView.ViewHolder(binding.root){
 
+        /**
+         * 적당한 색 모음
+         */
         private val colorList = intArrayOf(
             0xffef9a9a.toInt(), 0xffffe082.toInt(), 0xffc5e1a5.toInt(),
             0xff80cbc4.toInt(), 0xffb39ddb.toInt(), 0xff9fa8da.toInt(),
             0xffffcc80.toInt(), 0xffce93d8.toInt()
         )
 
+        /**
+         * view 에 mail 데이터를 입력한다.
+         * 프로필 이미지의 경우
+         * sender 의 이름 첫 글자가 알파벳일 경우 첫 글자를 표시한다.
+         * sender 의 이름 첫 글자가 한글일 경우 기본 프로필 아이콘을 표시한다.
+         * colorList 의 색 중 랜덤으로 하나를 뽑아 색을 덧씌운다.
+         */
         fun bind(item : Mail){
             binding.mail = item
             val initial = item.sender.toCharArray()[0]
@@ -36,6 +52,9 @@ class MailAdapter : RecyclerView.Adapter<MailAdapter.MailItemViewHolder>() {
         }
     }
 
+    /**
+     * 이전 목록과 새로 업데이트된 목록을 비교하여 알맞은 Notify 함수를 실행시키기 위한 DiffUtil.Callback
+     */
     class DiffUtilCallBack(
         private val oldList : List<Mail>,
         private val newList : List<Mail>
@@ -51,12 +70,16 @@ class MailAdapter : RecyclerView.Adapter<MailAdapter.MailItemViewHolder>() {
         }
     }
 
+    /**
+     * recyclerView 에 실질적으로 출력될 메일 목록
+     */
     private val mailList = mutableListOf<Mail>()
 
+    /**
+     * 메일 목록 업데이트 함수 DiffUtilCallBack 을 이용해
+     * 목록의 변화를 확인하고 View 를 업데이트한다.
+     */
     fun updateList(mails : List<Mail>){
-        /*mailList.clear()
-        mailList.addAll(mails)
-        notifyDataSetChanged()*/
         val diffUtilCallBack = DiffUtilCallBack(mailList,mails)
         val diffResult = DiffUtil.calculateDiff(diffUtilCallBack)
         mailList.run {
@@ -66,15 +89,24 @@ class MailAdapter : RecyclerView.Adapter<MailAdapter.MailItemViewHolder>() {
         }
     }
 
+    /**
+     * viewHolder 생성 함수
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MailItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.mail_item,parent,false)
         val binding = MailItemBinding.bind(view)
         return MailItemViewHolder(binding)
     }
 
+    /**
+     * viewHolder 의 bind 를 호출해 메일 목록의 메일을 View에 입력
+     */
     override fun onBindViewHolder(holder: MailItemViewHolder, position: Int) {
         holder.bind(mailList[position])
     }
 
+    /**
+     * 리스트의 아이템 개수를 반환
+     */
     override fun getItemCount(): Int  = mailList.size
 }
